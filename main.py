dea55f5671d60154e358a8ac41104022c73cd9e0
K Mahesh <mahesh0352002@gmail.com>
2023-05-20 16:13:41 +0530

    final commit

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
+from db import Database
+
+db = Database("Employee.db")
 root = Tk()
 root.title("Employee Management System")
-root.geometry("1920x1080+0+0")
+root.geometry("1980x1080+0+0")
 root.config(bg="#2c3e50") #can use color value as red,blue...instead of hexa value
 root.state("zoomed")
 
@@ -56,14 +60,69 @@ lblAddress.grid(row=4, column=0, padx=10, pady=10, sticky="w")
 txtAddress=Text(entries_frame,width=85,height=5,font=("Calibri",16))
 txtAddress.grid(row=5,column=0,columnspan=4,padx=10,sticky="w")
 
+
+def getData(event):
+    selected_row = tv.focus()
+    data = tv.item(selected_row)
+    global row
+    row = data["values"]
+    #print(row)
+    name.set(row[1])
+    age.set(row[2])
+    doj.set(row[3])
+    email.set(row[4])
+    gender.set(row[5])
+    contact.set(row[6])
+    txtAddress.delete(1.0, END)
+    txtAddress.insert(END, row[7])
+def dispalyAll():
+    tv.delete(*tv.get_children())
+    for row in db.fetch():
+        tv.insert("", END, values=row)
+
+
 def add_employee():
-    pass
+    if txtName.get() == "" or txtAge.get() == "" or txtDoj.get() == "" or txtEmail.get() == "" or comboGender.get() == "" or txtContact.get() == "" or txtAddress.get(
+            1.0, END) == "":
+        messagebox.showerror("Erorr in Input", "Please Fill All the Details")
+        return
+    db.insert(txtName.get(),txtAge.get(), txtDoj.get() , txtEmail.get() ,comboGender.get(), txtContact.get(), txtAddress.get(
+            1.0, END))
+    messagebox.showinfo("Success", "Record Inserted")
+    clearAll()
+    dispalyAll()
+
+
+
 def update_employee():
-    pass
+    if txtName.get() == "" or txtAge.get() == "" or txtDoj.get() == "" or txtEmail.get() == "" or comboGender.get() == "" or txtContact.get() == "" or txtAddress.get(
+            1.0, END) == "":
+        messagebox.showerror("Erorr in Input", "Please Fill All the Details")
+        return
+    db.update(row[0],txtName.get(), txtAge.get(), txtDoj.get(), txtEmail.get(), comboGender.get(), txtContact.get(),
+              txtAddress.get(
+                  1.0, END))
+    messagebox.showinfo("Success", "Record Update")
+    clearAll()
+    dispalyAll()
+
+
 def delete_employee():
-    pass
+    db.remove(row[0])
+    clearAll()
+    dispalyAll()
+
+
 def clearAll():
-    pass
+    name.set("")
+    age.set("")
+    doj.set("")
+    gender.set("")
+    email.set("")
+    contact.set("")
+    txtAddress.delete(1.0, END)
+
+
 btn_frame = Frame(entries_frame, bg="#535c68")
 btn_frame.grid(row=6, column=0, columnspan=4, padx=10, pady=10, sticky="w")
 btnAdd = Button(btn_frame, command=add_employee, text="Add Details", width=15, font=("Calibri", 16, "bold"), fg="white",
@@ -78,5 +137,32 @@ btnClear = Button(btn_frame, command=clearAll, text="Clear Details", width=15, f
                   bg="#f39c12",
                   bd=0).grid(row=0, column=3, padx=10)
 
+#Table Frame
+tree_frame=Frame(root,bg="#ecf0f1")
+tree_frame.place(x=0,y=480,width=1980,height=520)
+style = ttk.Style()
+style.configure("mystyle.Treeview", font=('Calibri', 18),
+                rowheight=50)  # Modify the font of the body
+style.configure("mystyle.Treeview.Heading", font=('Calibri', 18))  # Modify the font of the headings
+tv=ttk.Treeview(tree_frame,column=(1,2,3,4,5,6,7,8), style="mystyle.Treeview")
+tv.heading("1", text="ID")
+tv.column("1", width=5)
+tv.heading("2", text="Name")
+tv.heading("3", text="Age")
+tv.column("3", width=5)
+tv.heading("4", text="D.O.B")
+tv.column("4", width=10)
+tv.heading("5", text="Email")
+tv.heading("6", text="Gender")
+tv.column("6", width=10)
+tv.heading("7", text="Contact")
+tv.column("6", width=10)
+tv.heading("8", text="Address")
+tv['show']='headings'
+tv.bind("<ButtonRelease-1>", getData)
+tv.pack(fill=X)
 
+
+
+dispalyAll()
 root.mainloop()
